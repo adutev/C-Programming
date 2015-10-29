1cd406f9e1af9dfc27d7bfb8c93510ecb86cf0bc
adutev <adutev@gmail.com>
2015-10-29 19:07:56 +0200

    add 6

--- CArrays/CArrays/05_longest_increasing_sequence.c
+++ CArrays/CArrays/05_longest_increasing_sequence.c
@@ -7,12 +7,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <math.h>
 
-/*
- * 
- */
+int get_numbers(int*, int);
+void longest_increasing_sequence(int*,int);
+
 int main(int argc, char** argv) {
+    int i, size;
+    
+    printf("Enter an integer number:");
+    if(scanf("%d", &size) != 1) {
+        printf("Invalid input");
+        return 1;
+    }
+    int numbers[size];
+    int got_numbers = get_numbers(numbers, size);
+    if(got_numbers != 0){
+        printf("Invalid number of integers entered.");
+        return 1;
+    }
 
-    return (EXIT_SUCCESS);
+    longest_increasing_sequence(numbers, size);
+    
+    return 0;
 }
 
+int get_numbers(int* numbers, int size) {
+    int i, matches = 0;
+
+    printf("Enter %d integers numbers:", size);
+    for (i = 0; i < size; i++) {
+        matches += scanf("%d", &numbers[i]);
+    }
+    if (matches != size) {
+        printf("Invalid number of integers entered.");
+        return 1;
+    }
+    
+    return 0;
+}
+
+void longest_increasing_sequence(int* numbers,int size){
+    int i;
+    int curr_count = 1; //counts current seq
+    int curr_start = 0; //start of current seq
+    int max_start = 0; //start of the max seq
+    int max_count = 1; //max count
+    int in = 1;
+    
+    for(i = 1; i <= size; i++){
+        if(numbers[i-1] < numbers[i]){
+            if(!in){
+                in = 1;
+                curr_start = i - 1;
+            }
+            curr_count++;
+            printf("%d ", numbers[i-1]);
+        } else {
+            if(curr_count > max_count){
+                max_count = curr_count;
+                max_start = curr_start;
+            }
+            curr_start = i -1;
+            curr_count = 1;
+            in = 0;
+            printf("%d\n", numbers[i-1]);
+        }
+    }
+    printf("Longest: ");
+
+    for(i = max_start; i < (max_start + max_count);i++){
+        printf("%d ", numbers[i]);
+    }
+}
--- CArrays/CArrays/06_join_lists.c
+++ CArrays/CArrays/06_join_lists.c
@@ -1,9 +1,63 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
 
-int main()
-{
-	int n;
-	scanf("%d", &n);
-	return 0;
+/* 
+ * File:   06_join_lists.c
+ * Author: adutev
+ *
+ * Created on October 29, 2015, 6:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int get_numbers(int*, int);
+
+int main(int argc, char** argv) {
+    int i, j, size_a, size_b;
+    
+    printf("Enter a size for list a:");
+    if(scanf("%d", &size_a) != 1) {
+        printf("Invalid input");
+        return 1;
+    }
+    int list_a[size_a];
+     printf("\nEnter a size for list b:");
+    if(scanf("%d", &size_b) != 1) {
+        printf("Invalid input");
+        return 1;
+    }
+    int list_b[size_b];
+    int got_numbers_a = get_numbers(list_a, size_a);
+    int got_numbers_b = get_numbers(list_b, size_b);
+    if(got_numbers_a != 0 || got_numbers_b != 0){
+        printf("Invalid number of integers entered.");
+        return 1;
+    }
+    int size_result = size_a + size_b;
+    for(i = 0; i < size_a; i++){
+        for(j = 0; j< size_b; j++){
+            if(list_a[i] == list_b[j]){
+                printf("%d",list_a[i]);
+            }
+        }
+    }
+    
+    int result_list[size_result];
+
+    return 0;
+}
+
+int get_numbers(int* numbers, int size) {
+    int i, matches = 0;
+
+    printf("Enter %d integers numbers:", size);
+    for (i = 0; i < size; i++) {
+        matches += scanf("%d", &numbers[i]);
+    }
+    if (matches != size) {
+        printf("Invalid number of integers entered.");
+        return 1;
+    }
+    
+    return 0;
 }
